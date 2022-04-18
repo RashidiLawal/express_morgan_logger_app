6e9168160566898ad99baa37712a82e9e65f4604
lawal rashidi <lawalrashidi95@gmail.com>
2022-04-18 09:31:19 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const morgan = require("morgan");
+const {v4:uuidv4} = require("uuid");
+const fs = require("fs");
+const path = require("path")
+
+const app = express();
+const port = 3000;
+
+morgan.token('id', function getId(req){
+    return req.id
+})
+
+morgan.token('param', function(req, res, param){
+    return "userToken";
+})
+
+app.use(giveId);
+
+let accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags:'a'})
+
+app.use(morgan(':id "param:method :status :url "HTTP/:http-version"'));
+
+app.use(morgan(':id :param :method :status :url "HTTP/:http-version"', {stream:accessLogStream}));
+
+app.use(morgan(':param', {stream:accessLogStream}));
+
+
+app.get('/', (req, res) => {
+    res.end('Morgan Logger App')
+})
+
+function giveId(req, res, next){
+    req.id = uuidv4();
+    next();
+}
+
+app.listen(port, ()=>{console.log(`Server is running on http://localhost:${port}`)})
